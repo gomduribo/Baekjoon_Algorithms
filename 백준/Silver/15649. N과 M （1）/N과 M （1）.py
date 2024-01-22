0404d3530deb6e88997c90153de23546f39b25f4
Song Young Bo <72201661+gomduribo@users.noreply.github.com>
2024-01-22 23:28:08 +0900

    [Silver III] Title: N과 M (1), Time: 192 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,14 @@
+N,M=map(int, input().split(" "))
+ans=[]
+
+def back():
+  if len(ans)==M:
+    print(" ".join(map(str, ans)))
+
+  for i in range(1,N+1):
+    if i not in ans:
+      ans.append(i)
+      back()
+      ans.pop()
+
+back()
