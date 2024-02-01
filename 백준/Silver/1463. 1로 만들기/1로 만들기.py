eb9d3cee1bc256b6311965265a9980b15ec5948a
Song Young Bo <72201661+gomduribo@users.noreply.github.com>
2024-02-01 21:27:09 +0900

    [Silver III] Title: 1로 만들기, Time: 600 ms, Memory: 38932 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,15 @@
+N=int(input())
+
+num_list=[0]*1000001
+
+
+
+for i in range(2, N+1):
+  num_list[i]=num_list[i-1]+1
+  if i%2==0:
+    num_list[i]=min(num_list[i], num_list[i//2]+1)
+
+  if i%3==0:
+    num_list[i]=min(num_list[i], num_list[i//3]+1)
+
+print(num_list[N])
