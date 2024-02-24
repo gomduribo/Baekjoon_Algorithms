9c27d93b5234b4583b6e665da81fc78500d48aaf
Song Young Bo <72201661+gomduribo@users.noreply.github.com>
2024-02-24 16:45:06 +0900

    [Silver I] Title: 부등호, Time: 2648 ms, Memory: 33164 KB -BaekjoonHub

---
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -0,0 +1,21 @@
+import sys
+from itertools import permutations
+
+input=sys.stdin.readline
+k=int(input())
+E=list(map(str,input().split()))
+
+ans=[]
+num=[i for i in range(10)]
+
+for per in permutations(num, k+1):
+    for i in range(len(E)):
+        if E[i]=='<':
+            if per[i]>per[i+1]:break
+        else:
+            if per[i]<per[i+1]:break
+    else:
+        ans.append(per)
+ans.sort()
+print(''.join(map(str,ans[-1])))
+print(''.join(map(str,ans[0])))
