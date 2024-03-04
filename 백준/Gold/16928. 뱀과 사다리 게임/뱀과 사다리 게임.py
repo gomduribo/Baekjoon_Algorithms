dab4ee5e1296c9ca9e235dcfceeb709cd9508d0f
Song Young Bo <72201661+gomduribo@users.noreply.github.com>
2024-03-04 17:26:52 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 72 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -4,8 +4,6 @@ import time
 
 input=sys.stdin.readline
 
-# graph=[list(range(i,i+11)) for i in range(0,91,10) ]
-# print(graph)
 visited=[False]*101
 def BFS(ladders,snakes):
   q=deque()
@@ -14,39 +12,25 @@ def BFS(ladders,snakes):
   cnt=0
   result=[]
   while q:
-    # print(f"--{q}")
     cur=q.popleft()
-    # time.sleep(0.05)
-    # print(cur)
     if cur[0]==100:
       result.append(cur)
       break
       
-    # cnt+=1
     cnt=cur[1]+1
     for i in range(6):
       n_cur=cur[0]+d_cur[i]
 
       if n_cur>=0 and n_cur<=100:
-        # print("!!")
         new=n_cur
-        # for k in range(len(ladders)):
-        #   if ladders[k][0]==n_cur:
-        #     # print("ladder!")
-        #     new=ladders[k][1]
-        #     break
+        
         if n_cur in ladders.keys():
           new=ladders[n_cur]
-
-        # for j in range(len(snakes)):
-        #   if snakes[j][0]==n_cur:
-        #     # print("snake!")
-        #     new=snakes[j][1]
-        #     break
+   
         if n_cur in snakes.keys():
           new=snakes[n_cur]
         
-        # print(f"-->{(new,cnt)}")
+        
         if new==100:return cnt
 
         if not visited[new]:
@@ -59,12 +43,11 @@ N,M=map(int, input().split(" "))
 l=defaultdict(int)
 s=defaultdict(int)
 for _ in range(N):
-  # l.append(list(map(int, input().strip().split(" "))))
   x,y=map(int, input().strip().split(" "))
   l[x]=y
 
 for _ in range(M):
   u,v=map(int, input().strip().split(" "))
   s[u]=v
-# print(l)
+    
 print(BFS(l,s))
